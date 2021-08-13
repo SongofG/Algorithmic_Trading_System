9091c425ed31712518bd821de21a106b9a1d69ac
SongofG <steve121212@naver.com>
2021-08-13 16:12:55 +0900

    Algorithm 1 completely implemented.

--- Algorithms_1_trading_volume_analysis.py
+++ Algorithms_1_trading_volume_analysis.py
@@ -1,5 +1,6 @@
 # Building a function
 import win32com.client
+import time
 
 
 def CheckVolume(instStockChart, code):  # this method takes in the StockChart instance and the code as its parameter.
@@ -28,3 +29,18 @@ def CheckVolume(instStockChart, code):  # this method takes in the StockChart in
         return 1
     else:
         return 0
+
+
+if __name__ == "__main__":  # Run the code below only when it's ran by itself, not imported to other files.
+    instStockChart = win32com.client.Dispatch("CpSysDib.StockChart")
+    instCpCodeMgr = win32com.client.Dispatch("CpUtil.CpCodeMgr")
+    instCpStockCode = win32com.client.Dispatch("CpUtil.CpStockCode")
+    codeList = instCpCodeMgr.GetStockListByMarket(1)
+    buyList = []
+    start = time.time()
+    for code in codeList:
+        if CheckVolume(instStockChart, code) == 1:
+            buyList.append(code)
+            print(instCpStockCode.CodeToName(code))
+        time.sleep(0.75)
+    print('시간:', start - time.time())
