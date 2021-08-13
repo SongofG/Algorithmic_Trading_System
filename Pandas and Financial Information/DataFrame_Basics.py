e3de7eede3255b341037662f8c3ec2f08b6049c0
SongofG <steve121212@naver.com>
2021-08-13 21:17:23 +0900

    Pandas Basics: DataFrame

---
+++ Pandas and Financial Information/DataFrame_Basics.py
@@ -0,0 +1,50 @@
+# Python's dictionary -> DataFrame
+from pandas import Series, DataFrame
+
+raw_data = {'col0': [1, 2, 3, 4],
+            'col1': [10, 20, 30, 40],
+            'col2': [100, 200, 300, 400]}
+
+data = DataFrame(raw_data)  # This is pandas.DataFrame
+print(type(data))
+print(type(data['col0']))  # DataFrame is consisted of Series
+print(data)
+
+# Example1: Daeshin's stock prices
+daeshin = {'open': [11650, 11100, 11200, 11100, 11000],
+           'high': [12100, 11800, 11200, 11100, 11150],
+           'low': [11600, 11050, 10900, 10950, 10900],
+           'close': [11900, 11600, 11000, 11100, 11050]}
+
+daeshin_day = DataFrame(daeshin)
+print('\nExample1')
+print(daeshin_day)
+
+# Example 2
+daeshin = {'open': [11650, 11100, 11200, 11100, 11000],
+           'high': [12100, 11800, 11200, 11100, 11150],
+           'low': [11600, 11050, 10900, 10950, 10900],
+           'close': [11900, 11600, 11000, 11100, 11050]}
+
+daeshin_day = DataFrame(daeshin, columns=['open', 'high', 'low', 'close'])
+print('\nExample2')
+print(daeshin_day)
+
+# Example 3
+daeshin_day = DataFrame(daeshin,
+                        columns=['open', 'high', 'low', 'close'],
+                        index=['16.02.29', '16.02.26', '16.02.25', '16.02.24', '16.02.23'])
+print('\nExample3')
+print(daeshin_day)
+
+# .loc Should be used to access the row data
+day_data = daeshin_day.loc['16.02.24']
+print('\n')
+print(day_data)
+print(type(day_data))
+
+# Conclusion: To access rows, we need index. To access columns, we need the column names
+print('\n')
+print(daeshin_day.columns)
+print(daeshin_day.index)
+
